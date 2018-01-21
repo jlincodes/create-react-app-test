3a161bb793d5ba8a3026fde05a8387369c27f342
Julie <julie.lin0812@gmail.com>
2018-01-20 16:04:31 -0800

    Moves files

--- src/App.js
+++ src/App.js
@@ -7,15 +7,16 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1>Hello World</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
 }
 
+// <img src={logo} className="App-logo" alt="logo" />
+// <h1 className="App-title">Welcome to React</h1>
+// <p className="App-intro">
+//   To get started, edit <code>src/App.js</code> and save to reload.
+// </p>
 export default App;
